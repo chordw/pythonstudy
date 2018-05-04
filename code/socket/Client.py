ee2b27c5d55e8fc167eec23ec46e55bcf1fc09cd
wangxian <wangxianjl@sina.com>
2018-05-04 15:53:42 +0800

    添加微信库

--- code/socket/Client.py
+++ code/socket/Client.py
@@ -1,9 +1,13 @@
 # 文件名：Client.py
 import socket
-client = socket.socket()
-host = socket.gethostname()
-port = 15656
-client.connect((host, port))
-while True:
-    print(client.recv(1024))
-    client.close()
+try:
+    client = socket.socket()
+    host = socket.gethostname()
+    port = 15656
+    client.connect((host, port))
+    while True:
+        print(client.recv(1024))
+        client.close()
+except IOError as e:
+    print(e.args)
+    exit(1)
---
+++ it/_wx_itchat.py
@@ -0,0 +1,12 @@
+import itchat
+
+
+@itchat.msg_register(itchat.content.TEXT)
+def text_reply(msg):
+    return msg.text + ",999"
+
+
+itchat.auto_login(hotReload=True)
+# itchat.run()
+a = itchat.search_friends()
+print(a)
